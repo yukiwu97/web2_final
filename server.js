8e1432aaa912ac9af372e57f4ac7a668953edcd7
yukiwu97 <xwu5@sva.edu>
2020-04-30 17:07:00 -0400

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3005;
 // app.use(express.static(__dirname + '/public'));
 app.use(cors());
 app.use(express.json());
